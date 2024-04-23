cfd0343b47084021c082c7e7233e8fcee0406db3
Samuel Beckmann (RIT Student) <sbb7355@rit.edu>
2024-04-23 17:17:35 -0400

    Crediting and UI Text Update

--- Sam Beckmann AlterStaff Challenge/Assets/Scripts/Interactable.cs
+++ Sam Beckmann AlterStaff Challenge/Assets/Scripts/Interactable.cs
@@ -7,6 +7,7 @@ public class Interactable : MonoBehaviour
     // Establish necessary fields
     // Shared
     public bool isKey;
+    
 
     // Key Related Fields
     public int rewardLevel;
@@ -17,6 +18,10 @@ public class Interactable : MonoBehaviour
     public AnimationClip openClip;
     public AnimationClip closeClip;
     public bool isAnimPlaying = false; // Track this to ensure it does not spam animations.
+
+
+    // As a heads up, I made the key model and the animations for the chest, but I did not make the chest.
+    // Here is a link for where I got the chest from: "Treasure Chest Animation" (https://skfb.ly/o7vY7) by Matt Harris is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
 
     // Start is called before the first frame update
     void Start()
